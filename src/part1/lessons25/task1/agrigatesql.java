4e03e1ec22639f8b5f15f9a7adf35b71da69810b
ZaglubotskiyMN <zaglubotskii@gmail.com>
2021-03-01 11:54:08 +0400

    finaly

--- src/part1/lessons25/task1/agrigatesql.java
+++ src/part1/lessons25/task1/agrigatesql.java
@@ -1,21 +1,18 @@
 package part1.lessons25.task1;
 
-import javax.naming.spi.DirStateFactory;
 import java.sql.*;
 
-public class Selectall {
-
-    static final String DB_url= "jdbc:postgresql://127.0.0.1:5432/MyProject";
-    static final String login ="postgres";
+public class agrigatesql {
+    static final String DB_url = "jdbc:postgresql://127.0.0.1:5432/MyProject";
+    static final String login = "postgres";
     static final String pass = "hrnzpsods7753191";
 
-    public static void main(String[] args) throws ClassNotFoundException,SQLException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Statement statement = null;
         System.out.println("Testing PostgreSQL:");
         try {
             Class.forName("org.postgresql.Driver");
-        }catch (ClassNotFoundException e)
-        {
+        } catch (ClassNotFoundException e) {
             System.out.println("Driver error");
             e.printStackTrace();
             return;
@@ -23,42 +20,30 @@ public class Selectall {
         System.out.println("Driver -OK!");
         Connection conn = null;
         try {
-            conn = DriverManager.getConnection(DB_url,login,pass);
-        }catch (SQLException e)
-        {
+            conn = DriverManager.getConnection(DB_url, login, pass);
+        } catch (SQLException e) {
             System.out.println("Connection failed");
             e.printStackTrace();
             return;
         }
-        if (conn != null)
-        {
+        if (conn != null) {
             System.out.println("Connect - Ok!");
         } else {
             System.out.println("Not connection");
         }
 
         statement=conn.createStatement();
-        String sql ="SELECT * FROM history";
+        String sql ="SELECT MAX(count) FROM purchase_item";
         Boolean ok=statement.execute(sql);
         System.out.println("Успешен запрос:"+" "+ok);
         System.out.println("Выводим на экран");
         ResultSet resultSet=statement.executeQuery(sql);
         while (resultSet.next())
         {
-            int id = resultSet.getInt("id");
-            String product = resultSet.getString("product");
-            double old_price = resultSet.getDouble("old_price");
-            String createby = resultSet.getString("createby");
-
-            System.out.println("id:"+" "+id);
-            System.out.println("product:"+" "+product);
-            System.out.println("old_price:"+" "+old_price);
-            System.out.println("createby:"+" "+createby);
+            int max = resultSet.getInt("max");
+            System.out.println("max:"+" "+max);
             System.out.println("\n---------------");
-
         }
-
-
         try {
             resultSet.close();
             statement.close();
@@ -73,7 +58,5 @@ public class Selectall {
             e.printStackTrace();
             return;
         }
-
-
     }
 }
--- src/part1/lessons25/task1/wheresql.java
+++ src/part1/lessons25/task1/wheresql.java
@@ -1,21 +1,19 @@
 package part1.lessons25.task1;
 
-import javax.naming.spi.DirStateFactory;
 import java.sql.*;
 
-public class Selectall {
+public class wheresql {
 
-    static final String DB_url= "jdbc:postgresql://127.0.0.1:5432/MyProject";
-    static final String login ="postgres";
+    static final String DB_url = "jdbc:postgresql://127.0.0.1:5432/MyProject";
+    static final String login = "postgres";
     static final String pass = "hrnzpsods7753191";
 
-    public static void main(String[] args) throws ClassNotFoundException,SQLException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Statement statement = null;
         System.out.println("Testing PostgreSQL:");
         try {
             Class.forName("org.postgresql.Driver");
-        }catch (ClassNotFoundException e)
-        {
+        } catch (ClassNotFoundException e) {
             System.out.println("Driver error");
             e.printStackTrace();
             return;
@@ -23,41 +21,36 @@ public class Selectall {
         System.out.println("Driver -OK!");
         Connection conn = null;
         try {
-            conn = DriverManager.getConnection(DB_url,login,pass);
-        }catch (SQLException e)
-        {
+            conn = DriverManager.getConnection(DB_url, login, pass);
+        } catch (SQLException e) {
             System.out.println("Connection failed");
             e.printStackTrace();
             return;
         }
-        if (conn != null)
-        {
+        if (conn != null) {
             System.out.println("Connect - Ok!");
         } else {
             System.out.println("Not connection");
         }
 
         statement=conn.createStatement();
-        String sql ="SELECT * FROM history";
+        String sql ="SELECT * from city \n" +
+                "WHERE name = 'Москва'";
         Boolean ok=statement.execute(sql);
         System.out.println("Успешен запрос:"+" "+ok);
         System.out.println("Выводим на экран");
         ResultSet resultSet=statement.executeQuery(sql);
         while (resultSet.next())
         {
-            int id = resultSet.getInt("id");
-            String product = resultSet.getString("product");
-            double old_price = resultSet.getDouble("old_price");
-            String createby = resultSet.getString("createby");
-
-            System.out.println("id:"+" "+id);
-            System.out.println("product:"+" "+product);
-            System.out.println("old_price:"+" "+old_price);
-            System.out.println("createby:"+" "+createby);
+            int city_id = resultSet.getInt("city_id");
+            String name = resultSet.getString("name");
+            int timezone_id = resultSet.getInt("timezone_id");
+            System.out.println("city_id:"+" "+city_id);
+            System.out.println("name:"+" "+name);
+            System.out.println("timezone_id:"+" "+timezone_id);
             System.out.println("\n---------------");
 
         }
-
 
         try {
             resultSet.close();
